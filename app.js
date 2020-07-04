e2470933b3f4a623af8b16e1fa76e326b2d03ffc
raihanahmad01 <dev.raihanahmad@gmail.com>
2020-07-04 20:17:48 +0600

    JavaScript add and project works

---
+++ app.js
@@ -0,0 +1,57 @@
+// declaring dom varible
+let latitude = document.querySelector("#lat");
+let longitude = document.querySelector("#lon");
+let altitude = document.querySelector("#alt");
+
+// calling api request
+fetch("https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/1/&apiKey=B5TK9V-KXSGG6-JUXT3N-497X#")
+  .then(res => res.json())
+  .then(data => data.positions[0])
+  .then(data => {
+
+    let x = data.satlatitude;
+    let y = data.satlongitude;
+    let z = data.sataltitude;
+    console.log(data);
+
+    // changing dom latitude & longitude value
+    latitude.innerHTML = `Latitude: ${x}`;
+    longitude.innerHTML = `Longitude: ${y}`;
+    altitude.innerHTML = `Altitude: ${z} KM`;
+
+    // making map
+    let baseMapLayer = new ol.layer.Tile({
+      source: new ol.source.OSM()
+    });
+    let map = new ol.Map({
+      target: 'map',
+      layers: [baseMapLayer],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([y, x]),
+        zoom: 4 //Initial Zoom Level
+      })
+    });
+    // making map marker
+    let marker = new ol.Feature({
+      geometry: new ol.geom.Point(
+        ol.proj.fromLonLat([y, x])
+      ), // Cordinates of ISS
+    });
+
+    marker.setStyle(new ol.style.Style({
+      image: new ol.style.Icon(({
+        color: 'rgba(255,255,255,0)',
+        crossOrigin: '',
+        src: 'https://raw.githubusercontent.com/nasimuddin01/iss-tracker/master/iss-new.png'
+      }))
+    }));
+
+    let vectorSource = new ol.source.Vector({
+      features: [marker]
+    });
+    let markerVectorLayer = new ol.layer.Vector({
+      source: vectorSource,
+    });
+    map.addLayer(markerVectorLayer);
+
+  });
